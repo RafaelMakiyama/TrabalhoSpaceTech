538ad1d9200a9825177cbca3d081294577a8ecb3
filipesander <filipesander777@gmail.com>
2021-12-02 10:54:04 -0300

    autoload

--- Exercicio_1/autoload.php
+++ Exercicio_1/autoload.php
@@ -1,7 +1,19 @@
 <?php
-    function pegaClasse($nomeClasse){
-        require_once($nomeClasse . ".php");
-    }
+spl_autoload_register(
+    
+    function($nomeDaClasse){
 
-    spl_autoload_register("pegaClasse");
-?>
+        $directory = array('Classes');
+
+        foreach ($directory as $current_dir)
+        {
+            $caminho = $current_dir.DIRECTORY_SEPARATOR.$nomeDaClasse . '.php';
+
+            if (file_exists($caminho) ) {
+                require_once($caminho);
+            }
+
+        }
+    }
+    
+);
