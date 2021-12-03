2de6269ce459b039bf3e4fb90353ca992738b43f
felipe.neves34@etec.sp.gov.br <felipe.neves@soluti.com.br>
2021-12-03 10:36:21 -0300

    Metodo mostrarStreaming implementado em North e Full

--- Exercicio_2/Bridge/Classes/FullAnimations.php
+++ Exercicio_2/Bridge/Classes/FullAnimations.php
@@ -6,4 +6,9 @@ class FullAnimations extends Streaming{
     {  
        return $this->conexao->connection(); 
     }
+
+    public function mostrarStreaming()
+    {
+        return "Você está conectado na FullAnimations";
+    }
 }
