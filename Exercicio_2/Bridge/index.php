a02a5c4424312b41728afc2e342a86b6fb005a1c
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-03 10:48:14 -0300

    Criado index do modo Bridge

--- Exercicio_2/Bridge/index.php
+++ Exercicio_2/Bridge/index.php
@@ -1,5 +1,65 @@
 <?php
 require_once('autoload.php');
+echo '<h2>Streamings e seus respectivos databases:</h2>';
 
-$servico = new Netflixxx33(new Mysql);
- $servico->conexaoBancoDeDados();
+$servico = new DisneyPlus(new Oracle);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new Netflixxx33(new Postgres);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new DarkFilmes(new Mongo);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new FullAnimations(new MariaDB);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new AnimaKaiTv2(new Postgres);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new SkyPobre(new Oracle);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new PlusOne(new Mongo);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new AcademicGreen(new FireBase);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new NorthTv(new FireBase);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new TelgoPlus(new Mysql);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new AvianPlus(new Mongo);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+echo '<br><h2>Streamings conectando com diferentes databases</h2>';
+
+$servico = new SkyPobre(new Mysql);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new NorthTv(new Oracle);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new DisneyPlus(new Postgres);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
+
+$servico = new AvianPlus(new FireBase);
+echo $servico->mostrarStreaming();
+$servico->conexaoBancoDeDados();
