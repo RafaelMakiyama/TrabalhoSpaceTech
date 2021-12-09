c78c0101c203599c948ed66a7dd03f7eac333001
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-09 11:55:43 -0300

    ajustado view

--- app/resources/views/item/index.blade.php
+++ app/resources/views/item/index.blade.php
@@ -1,11 +1,34 @@
-<h1>Lista de item</h1>
-
-<ul>
-@foreach ($items as $k => $item )
-    <h3> item {{$k +1}}</h3>
-    <li>{{$item->id}}</li>
-    <li>{{$item->name}}</li>
-    <a href=""> Clique aqui</a>
-@endforeach    
-
+<h1>Lista de items:</h1>
+    <table>
+        <tr>
+            <th>Codigo</th>
+            <th>Nome do Item</th>
+            <th>Ação</th>
+        </tr>
+    @foreach($items as $item)
+        <tr>
+          <th>{{ $item['id'] }}</th>
+          <th>{{ $item['name'] }}</th>          
+          {{-- <th><a href={{route('items.show', $item['id'])}}>Exibir item</a></th> --}}
+        </tr>
+    @endforeach
+    </table>
+    <br><br>
 </ul>
+<style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+    
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+</style>
