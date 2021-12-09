4e3f80ea0ef2d032811299cbf652e07fe9963eb2
Rafael Makiyama <rafael.makiyama@soluti.com.br>
2021-12-09 10:20:39 -0300

    Estruturando o projeto

---
+++ app/app/Models/Timetable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Timetable extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'day'
+    ];
+
+    protected $cats =[
+        'entry_time'=> 'time',
+        'out_time'=> 'time'
+    ];
+
+    
+}
---
+++ app/database/migrations/2021_12_09_131029_create_timetables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('day');
+            $table->time('entry_time');
+            $table->time('out_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
