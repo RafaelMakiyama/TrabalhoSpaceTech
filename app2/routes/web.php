3a3d44610c5319dda3074c4a96972499527fcab1
Rafael Makiyama <rafael.makiyama@soluti.com.br>
2021-12-14 16:59:53 -0300

    REsolvendo conflito

--- app2/routes/web.php
+++ app2/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\BloodBankController;
-use App\Http\Controllers\DoctorController;
-=======
 use App\Http\Controllers\{DoctorController, PatientController};
->>>>>>> f07eb6cb6308dc572c2323df5f3aee0a75a12ff8
 use Illuminate\Support\Facades\Route;
 
 /*
