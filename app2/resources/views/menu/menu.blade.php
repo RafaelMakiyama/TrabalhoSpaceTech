70b685e3cefabe32e9c95c71dffae7e922ea896c
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-14 17:36:55 -0300

    Ajuste Menu

--- app2/resources/views/menu/menu.blade.php
+++ app2/resources/views/menu/menu.blade.php
@@ -4,5 +4,5 @@
     <li><a class="{{Route::is('banco-de-sangue') ? 'active': ''}}" href="{{ route('banco-de-sangue.index')}}">Banco de Sangue</a></li>
     <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{ route('home')}}">Estagiários</a></li>
     <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{ route('home')}}">Doações</a></li>
-    <li><a class="{{Route::is('pacientes.index') ? 'active': ''}}" href="{{ route('pacientes.index')}}">Pacientes</a></li>
+    <li><a class="{{(Route::is('pacientes.index')||Route::is('pacientes.create')) ? 'active': ''}}" href="{{route('pacientes.index')}}">Pacientes </a></li>
 </ul>
