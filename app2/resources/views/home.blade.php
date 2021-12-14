f77164a003c8a60df38a57e3883c49fdbfbd373b
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-14 13:32:03 -0300

    Estruturas Layout e Menu

---
+++ app2/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+@section('pageTitle', 'Página Inicial')
+@include('menu.menu')
+@section('body')
+<br>
+<h1 class="title text-center">Hospital São lorenzo</h1>
+<p class="text-center">Clique em um item no menu acima para ver os detalhes</p>
+@endsection
---
+++ app2/resources/views/menu/menu.blade.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Médicos</a></li>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Doadores</a></li>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Banco de Sangue</a></li>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Estagiários</a></li>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Doações</a></li>
+    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Pacientes</a></li>
+</ul>
