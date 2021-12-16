178ac6c70886dd61d69d19f84d9d599e346179f4
filipesander <filipesander777@gmail.com>
2021-12-16 17:30:28 -0300

    corrigido placeholder form de estagiarios

--- app2/resources/views/trainees/create.blade.php
+++ app2/resources/views/trainees/create.blade.php
@@ -4,9 +4,7 @@
 @include('layouts.alerts')
 
 <form method="POST" action="{{ route('estagiarios.store')}}">
-
-    
-
+@include('trainees.form')
     <button type="submit" class="btn btn-primary">Cadastrar</button>
   </form>
 
