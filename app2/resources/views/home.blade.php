1f16fcaeec2e5df6a109fb8cea0216791a3c501a
FelipeSNeves <felipe.s.neves@outlook.com>
2021-12-17 10:54:24 -0300

    Melhorando o nome do hospital

--- app2/resources/views/home.blade.php
+++ app2/resources/views/home.blade.php
@@ -2,6 +2,6 @@
 @section('pageTitle', 'Página Inicial')
 @section('body')
 <br>
-<h1 class="title text-center">Hospital São lorenzo</h1>
+<h1 class="title text-center">Hospital São Lorenzo</h1>
 <p class="text-center">Clique em um item no menu acima para ver os detalhes</p>
 @endsection
