40889b0053d66f797ea35b39474fa112794918af
GABRIEL ANADINHO <46759731+Anadinho@users.noreply.github.com>
2021-12-23 14:46:20 -0300

    model Lesson

--- app3/app/Models/Lesson.php
+++ app3/app/Models/Lesson.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+    protected $fillable = 
+    [
+        'num_lesson',
+        'theme',
+        'duration',
+        'obs',
+        'cpf',
+        'course_id',
+        'financial_plan_id'
+    ]; 
+
+    public function Teacher(){
+        return $this->hasOne(Teacher::class, 'id','teacher_id');
+    }
+
+    public function Courses(){
+        return $this->hasOne(Courses::class,'id','course_id');
+    }
 }
