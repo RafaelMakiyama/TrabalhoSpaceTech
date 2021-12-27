26c5adb377828bf2b20b37bc24261c3f545374f5
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-27 18:02:14 -0300

    Ajustes Login e Logout

--- app3/resources/views/home.blade.php
+++ app3/resources/views/home.blade.php
@@ -3,5 +3,9 @@
 @section('body')
 <br>
 <h1 class="title text-center">Sistema de Gerenciamento</h1>
+@auth
 <p class="text-center">Clique em um item no menu acima para ver os detalhes</p>
+@else
+<p class="text-center">Entre no sistema para continuar</p>
+@endauth
 @endsection
--- app3/resources/views/menu/menu.blade.php
+++ app3/resources/views/menu/menu.blade.php
@@ -1,35 +1,52 @@
 <ul>
-    <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Início</a></li>
-    <li><a class="{{(
-        Route::is('professor.index')||
-        Route::is('professor.create')||
-        Route::is('professor.edit')||
-        Route::is('professor.show')
-        ) ? 'active': ''}}" href="{{route('professor.index')}}">Professores</a></li>
+    @auth
+        <li><a class="{{Route::is('home') ? 'active': ''}}" href="{{route('home')}}">Início</a></li>
+        <li><a class="{{(
+            Route::is('professor.index')||
+            Route::is('professor.create')||
+            Route::is('professor.edit')||
+            Route::is('professor.show')
+            ) ? 'active': ''}}" href="{{route('professor.index')}}">Professores</a></li>
 
-    <li><a class="{{(
-        Route::is('alunos.index')||
-        Route::is('alunos.create')||
-        Route::is('alunos.edit')||
-        Route::is('alunos.show')
-        ) ? 'active': ''}}" href="{{route('alunos.index')}}">Alunos</a></li>
+        <li><a class="{{(
+            Route::is('alunos.index')||
+            Route::is('alunos.create')||
+            Route::is('alunos.edit')||
+            Route::is('alunos.show')
+            ) ? 'active': ''}}" href="{{route('alunos.index')}}">Alunos</a></li>
 
-    <li><a class="{{
-        Route::is('cursos.index') ||
-        Route::is('cursos.create')||
-        Route::is('cursos.edit') ||
-        Route::is('cursos.show') ? 'active': ''}}" href="{{ route('cursos.index')}}">Cursos</a></li>
+        <li><a class="{{
+            Route::is('cursos.index') ||
+            Route::is('cursos.create')||
+            Route::is('cursos.edit') ||
+            Route::is('cursos.show') ? 'active': ''}}" href="{{ route('cursos.index')}}">Cursos</a></li>
 
-    <li><a class="{{(  Route::is('planos-financeiro.index')||
-        Route::is('planos-financeiro.create')||
-        Route::is('planos-financeiro.edit')||
-        Route::is('planos-financeiro.show')
-        ) ? 'active': ''}}" href="{{route('planos-financeiro.index')}}">Planos Financeiro</a></li>
+        <li><a class="{{(  Route::is('planos-financeiro.index')||
+            Route::is('planos-financeiro.create')||
+            Route::is('planos-financeiro.edit')||
+            Route::is('planos-financeiro.show')
+            ) ? 'active': ''}}" href="{{route('planos-financeiro.index')}}">Planos Financeiro</a></li>
 
-    <li><a class="{{(  Route::is('aulas.index')||
-        Route::is('aulas.create')||
-        Route::is('aulas.edit')||
-        Route::is('aulas.show')
-        ) ? 'active': ''}}" href="{{route('aulas.index')}}">Aulas</a></li>
-    <li>
+        <li><a class="{{(  Route::is('aulas.index')||
+            Route::is('aulas.create')||
+            Route::is('aulas.edit')||
+            Route::is('aulas.show')
+            ) ? 'active': ''}}" href="{{route('aulas.index')}}">Aulas</a></li>
+        <li> 
+        <li style="float: right">
+            <form action="{{route('logout')}}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-danger">Sair</button>
+            </form>
+        </li>
+    @else
+        <li>
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Entrar</a>
+        </li>
+    @if (Route::has('register'))
+        <li>
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar</a>
+        </li>
+    @endif
+    @endauth
 </ul>
