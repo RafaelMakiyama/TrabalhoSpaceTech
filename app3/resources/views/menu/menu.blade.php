7d2bb130b8d5016a68c5a559c343a6a5b346f620
Rafael Makiyama <rafael.makiyama@soluti.com.br>
2021-12-28 16:08:09 -0300

    conflito no menu

--- app3/resources/views/menu/menu.blade.php
+++ app3/resources/views/menu/menu.blade.php
@@ -2,21 +2,6 @@
 
     @auth
 
-        <li><a class="{{(  Route::is('aulas.index')||
-            Route::is('aulas.create')||
-            Route::is('aulas.edit')||
-            Route::is('aulas.show')
-            ) ? 'active': ''}}" href="{{route('aulas.index')}}">Aulas</a></li>
-        <li> 
-        <li style="float: right">
-            <form action="{{route('logout')}}" method="post">
-                @csrf
-                <button type="submit" class="btn btn-danger" style="padding: 13px;">Sair</button>
-            </form>
-        </li>
-        <li style="float: right">
-            <a href="#">Olá {{Auth::user()->name}}</a>
-        </li>
     @else
     <ul>
         <li>
