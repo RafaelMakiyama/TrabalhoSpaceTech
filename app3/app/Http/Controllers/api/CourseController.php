d645da775346fb59206b17be4ba558027c730b7d
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-28 15:20:14 -0300

    filtragem de cursos por campos API

--- app3/app/Http/Controllers/api/CourseController.php
+++ app3/app/Http/Controllers/api/CourseController.php
@@ -4,16 +4,30 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Repository\CourseRepository;
+use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    
-    public function courses_status($status)
+    private $model;
+
+    public function __construct(Course $model)
     {
+        $this->model = $model;
+    }
+    
+    public function courses_status(Request $request, $status)
+    {
+        $courses = $this->model;
+        if($request->has('campos'))
+        {
+            $courseRepository = new CourseRepository($courses);
+            $courses = $courseRepository->filterFields($request);
+        }
         if($status=='Ativado')
-            $courses = Course::where('status', '=', 'Ativado')->get();
+            $courses = $courses->where('status', '=', 'Ativado')->get();
         else if($status=='Desativado')
-            $courses = Course::where('status', '=', 'Desativado')->get();
+            $courses = $courses->where('status', '=', 'Desativado')->get();
         else
             return response()->json(['message' => 'Status nao encontrado'], 404);
         return response()->json($courses);
