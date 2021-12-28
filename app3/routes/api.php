90611d23f94e49c3fc08fe36d7f8bb730b74f13d
FelipeSNeves <felipe.s.neves@outlook.com>
2021-12-28 17:07:00 -0300

    Refatorando auth.basic

--- app3/routes/api.php
+++ app3/routes/api.php
@@ -19,10 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/alunos/curso/{curso}', [StudentController::class,'students_by_course'])->name('alunos.curso')->middleware('auth.basic');
-Route::get('/alunos/{aluno}/aulas', [StudentController::class,'lessons_by_student'])->name('alunos.aulas')->middleware('auth.basic');
-Route::get('/professores', [TeacherController::class, 'index'])->name('professores.index')->middleware('auth.basic');
-Route::get('/professores/{professor}', [TeacherController::class, 'show'])->name('professores.show')->middleware('auth.basic');
-Route::get('/cursos/{status}', [CourseController::class, 'courses_status'])->name('cursos.status')->middleware('auth.basic');
-Route::get('/aulas', [LessonController::class, 'index'])->name('aulas.show')->middleware('auth.basic');
-Route::get('/aulas/curso/{curso}', [LessonController::class, 'lesson_course'])->name('aulas_curso.show')->middleware('auth.basic');
+Route::middleware('validarpermissao')->group(function () {
+    
+    Route::get('/alunos/curso/{curso}', [StudentController::class,'students_by_course'])->name('alunos.curso');
+    Route::get('/alunos/{aluno}/aulas', [StudentController::class,'lessons_by_student'])->name('alunos.aulas');
+    Route::get('/professores', [TeacherController::class, 'index'])->name('professores.index');
+    Route::get('/professores/{professor}', [TeacherController::class, 'show'])->name('professores.show');
+    Route::get('/cursos/{status}', [CourseController::class, 'courses_status'])->name('cursos.status');
+    Route::get('/aulas', [LessonController::class, 'index'])->name('aulas.show');
+    Route::get('/aulas/curso/{curso}', [LessonController::class, 'lesson_course'])->name('aulas_curso.show');
+
+});
