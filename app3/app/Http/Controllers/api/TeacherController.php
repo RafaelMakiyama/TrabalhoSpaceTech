a5c6e9098dcb3fac7c2874a3953f7b3633bd02f9
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-28 11:00:08 -0300

    Endpoint Professores API

---
+++ app3/app/Http/Controllers/api/TeacherController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $teachers = Teacher::all();
+        return response()->json($teachers);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $teacher = Teacher::find($id);
+        if(!$teacher){
+            return response()->json(['message' => 'Professor nao encontrado'], 404);
+        }
+        return response()->json($teacher);
+    }
+
+}
--- app3/routes/api.php
+++ app3/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\StudentController;
+use App\Http\Controllers\api\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('/alunos', StudentController::class);
+Route::get('/professores', [TeacherController::class, 'index'])->name('professores.index');
+Route::get('/professores/{professor}', [TeacherController::class, 'show'])->name('professores.show');
